2102f0285573721ad4bdc85212240e73662f8d7e
Jonas Raoni Soares da Silva <github@raoni.org>
2026-04-28 14:31:04 +0200

    #19 Enabled the configuration of multiple disposable domains

--- MailSendFilterPlugin.php
+++ MailSendFilterPlugin.php
@@ -20,6 +20,7 @@ use APP\plugins\generic\mailSendFilter\classes\DownloadHandler;
 use APP\plugins\generic\mailSendFilter\classes\MailFilter;
 use APP\plugins\generic\mailSendFilter\classes\MailManager;
 use APP\plugins\generic\mailSendFilter\classes\SettingsForm;
+use APP\plugins\generic\mailSendFilter\classes\migration\I19_UpdateDisposableDomainsUrls;
 use Illuminate\Database\Query\Builder;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
@@ -265,5 +266,13 @@ class MailSendFilterPlugin extends GenericPlugin
     public function getInstallSitePluginSettingsFile(): string
     {
         return $this->getPluginPath() . '/settings.xml';
+    }
+
+    /**
+     * @copydoc Plugin::getInstallMigration()
+     */
+    public function getInstallMigration()
+    {
+        return new I19_UpdateDisposableDomainsUrls();
     }
 }
